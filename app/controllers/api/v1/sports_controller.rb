3d7e010ce25660af7fe073264cc38a66508b7118
Renata <renatateixeira.est@gmail.com>
2022-05-25 10:36:07 -0300

    refactor: Reconfigurei as rotas adicionando a versão da API (v1)

---
+++ app/controllers/api/v1/sports_controller.rb
@@ -0,0 +1,55 @@
+module Api
+  module V1
+    class SportsController < ApplicationController
+      before_action :set_sport, only: [:show, :update, :destroy]
+    
+      # GET /sports or /sports.json
+      def index
+        @sports = Sport.all
+    
+        render json: @sports
+      end
+    
+      # GET /sports/1 or /sports/1.json
+      def show
+        render json: @sport
+      end
+    
+      # POST /sports or /sports.json
+      def create
+        @sport = Sport.new(sport_params)
+    
+        if @sport.save
+          render json: @sport, status: :created, location: api_v1_sport_url(@sport)
+        else
+          render json: @sport.errors, status: :unprocessable_entity
+        end
+      end
+    
+      # PATCH/PUT /sports/1 or /sports/1.json
+      def update
+        if @sport.update(sport_params)
+          render json: @sport
+        else
+          render json: @sport.errors, status: :unprocessable_entity
+        end
+      end
+    
+      # DELETE /sports/1 or /sports/1.json
+      def destroy
+        @sport.destroy
+      end
+    
+      private
+        # Use callbacks to share common setup or constraints between actions.
+        def set_sport
+          @sport = Sport.find(params[:id])
+        end
+    
+        # Only allow a list of trusted parameters through.
+        def sport_params
+          params.require(:sport).permit(:name)
+        end
+    end
+  end
+end
---
+++ app/controllers/api/v1/teams_controller.rb
@@ -0,0 +1,64 @@
+module Api
+  module V1
+    class TeamsController < ApplicationController
+      before_action :set_team, only: [:show, :update, :destroy]
+    
+      # GET /teams or /teams.json
+      def index
+        @teams = Team.all
+    
+        render json: @teams
+      end
+    
+      # GET /teams/1 or /teams/1.json
+      def show
+        render json: @teams
+      end
+    
+      # GET /teams/new
+      # def new
+      #   @team = Team.new
+      # end
+    
+      # GET /teams/1/edit
+      # def edit
+      # end
+    
+      # POST /teams or /teams.json
+      def create
+        @team = Team.new(team_params)
+    
+        if @team.save
+          render json: @team, status: :created, location: api_v1_team_url(@team)
+        else
+          render json: @team.errors, status: :unprocessable_entity
+        end
+      end
+    
+      # PATCH/PUT /teams/1 or /teams/1.json
+      def update
+        if @team.update(team_params)
+          render json: @team
+        else
+          render json: @team.errors, status: :unprocessable_entity
+        end
+      end
+    
+      # DELETE /teams/1 or /teams/1.json
+      def destroy
+        @team.destroy
+      end
+    
+      private
+        # Use callbacks to share common setup or constraints between actions.
+        def set_team
+          @team = Team.find(params[:id])
+        end
+    
+        # Only allow a list of trusted parameters through.
+        def team_params
+          params.require(:team).permit(:userId, :sportId)
+        end
+    end
+  end
+end
